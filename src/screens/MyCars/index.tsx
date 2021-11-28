28e9fe030fd7fe2864fecc0f5eb4e5d5b9d17d1d
Yanderson Sena <contato@yandev.com.br>
2021-11-28 15:16:31 -0300

    Just Commit

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,18 +1,40 @@
 import React,{ useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { FlatList, StatusBar, Text } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
 import { CarDTO } from '../../dtos/carDTO';
 import { api } from '../../services/api';
 import {
-    Container
+    Container,
+    Header,
+    Title,
+    SubTitle,
+    Content,
+    Appoitments,
+    AppoitmentsTitle,
+    AppoitmentsQuantity,
 } from './styles';
+import { Car } from '../../components/Car';
 
-interface Props {
-
+interface CarProps {
+    id: string;
+    user_id: string
+    car: CarDTO;
 }
 
 export function MyCars() {
-    const [ cars, setCars ] = useState<CarDTO[]>([]);
+    const [ cars, setCars ] = useState<CarProps[]>([]);
     const [ loading, setLoading ] = useState(true);
+
+    const { colors } = useTheme();
+    const navigation = useNavigation();
+
+
+    function handleBack(){
+        navigation.goBack();
+    }
 
     useEffect(() => {
         async function fetchCars() {
@@ -27,9 +49,44 @@ export function MyCars() {
         } 
 
         fetchCars();
-     },[])
+     },[]);
+
     return (
         <Container>
+            <Header>
+                <StatusBar 
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton onPress={handleBack} color={colors.shape}/>
+
+                <Title>
+                    Escolha uma {'\n'} 
+                    data de inicio e {'\n'}
+                    fim do aluguel
+                </Title>
+                
+                <SubTitle>
+                    Conforto, segurança e praticidade
+                </SubTitle>
+            </Header>
+
+            <Content>
+                <Appoitments>
+                    <AppoitmentsTitle>Agendamentos Feitos</AppoitmentsTitle>
+                    <AppoitmentsQuantity>05</AppoitmentsQuantity>
+                </Appoitments>
+
+                <FlatList 
+                    data={cars}
+                    keyExtractor={item  => item.id}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({item}) => (
+                        <Car data={item.car}/>
+                    )}
+                />
+            </Content>
 
         </Container>
     );
